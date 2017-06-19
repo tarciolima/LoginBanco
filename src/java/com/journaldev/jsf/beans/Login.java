99e4b3d33468af136c02310c9478aee629aa5e6f
tarciolima <tarcio.nina@gmail.com>
2017-06-19 14:38:35 -0300

    Comit inicial

---
+++ src/java/com/journaldev/jsf/beans/Login.java
@@ -0,0 +1,79 @@
+package com.journaldev.jsf.beans;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import com.journaldev.jsf.dao.LoginDAO;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Tarcio
+ */
+@ManagedBean
+@SessionScoped
+@Entity
+
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1094801825228386363L;
+	
+        @Id
+	private String pwd;
+	private String msg;
+	private String user;
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	//validate login
+	public String validateUsernamePassword() {
+		boolean valid = LoginDAO.validate(user, pwd);
+		if (valid) {
+			HttpSession session = SessionUtils.getSession();
+			session.setAttribute("username", user);
+			return "admin";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN,
+							"Uauário ou Senha Incorreto",
+							"Por favor digite o usuário e senha corretos!"));
+			return "login";
+		}
+	}
+
+	//logout event, invalidate session
+	public String logout() {
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return "login";
+	}
+}
